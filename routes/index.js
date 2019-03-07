2e85611ed69cce24e297b4212f702115d042eaec
xiaoqiuxiong <563995050@qq.com>
2019-03-07 21:18:42 +0800

    20190307211742

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ router.get('/', async (ctx, next) => {
     await ctx.render('index', { title: '首页', mainTitle: '我的桌面', subTitle: '', userInfo: ctx.userInfo });
 })
 
+router.get('/welcome', async (ctx, next) => {
+    await ctx.render('welcome', { title: '首页', mainTitle: '我的桌面', subTitle: '', userInfo: ctx.userInfo });
+})
+
 module.exports = router
